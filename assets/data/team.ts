ac07136284c749863565794e49fd313e7a02671c
JustKocar <unclekocar@gmail.com>
2022-12-03 21:03:48 +0100

    txt

--- assets/data/team.ts
+++ assets/data/team.ts
@@ -7,32 +7,34 @@ export const members = [
   },
   {
     name: 'Dalibor Jelínek',
-    role: 'Filmmaker, Fotograf, Zvukař',
+    role: 'Filmmaker, Fotograf',
     description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Integer vulputate sem a nibh rutrum consequat. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris metus.',
     tag: 'Dali'
   },
   {
     name: 'Denisa Klimešová',
     role: 'Fotografka',
-    description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Integer vulputate sem a nibh rutrum consequat. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris metus.',
+    description: 'Jmenuji se Denča a jsem fotící maniak, co nejradši zachycuje přirozenost, radost, emoce, lásku a krásu takovou, jaká ve skutečnosti je. Jsem ta, co má stále úsměv od ucha k uchu a nechybí jí smysl pro humor. Vystudovala jsem střední uměleckou školu se zaměřením na fotografii, a tak se fotografování věnuji již od svých patnácti let. Zkušenosti mám především se svatebním, rodinným či reportážním focením. Ráda hledám detail v objektivu i v osobách před ním. Zkrátka potřebuji cítit spojení, aby mé fotografie vyprávěly příběh, ve kterém se sejdou vize klienta s těmi mými.
+    ',
     tag: 'Denca'
   },
   {
     name: 'Anna Zahálková',
-    role: 'PR, Sociální sítě',
+    role: 'PR master',
     description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Integer vulputate sem a nibh rutrum consequat. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris metus.',
     tag: 'Anicka'
   },
   {
     name: 'Matyáš Vrba',
     role: 'Streamer',
-    description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Integer vulputate sem a nibh rutrum consequat. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris metus.',
+    description: 'Zdravím, jsem Matyáš a v Kocar Production mám na starosti především živé vysílání. Mezi mé nejcennější zkušenosti patří mezinárodní stream středoevropského literárního festivalu Měsíc autorského čtení, který již přes dvacet let každoročně pořádá nakladatelství Větrné mlýny, a kde se o živý přenos stará KoPro už třetím rokem. Mezi mé nejnovější zkušenosti pak patří například streamování a záznam kulturních večerů probíhajících v Rezidenci Café Kaprál, probíhajících pod záštitou TIC Brno.',
     tag: 'Maty'
   },
   {
     name: 'Šárka Portešová',
-    role: 'Grafický design',
-    description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Integer vulputate sem a nibh rutrum consequat. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris metus.',
+    role: 'Grafická designerka',
+    description: 'Jmenuju se Šárka a v KoPro mám na starost grafický desig, kterému jsem se začala věnovat již na gymnáziu. Podílela jsem se zde na různorodých projektech počínaje tvorbou plakátů pro různé biologické olympiády až po návrh vizuálního stylu na školní ples. Nyní studuji na fakultě informatiky MU vizuální informatiku, která zahrnuje právě i grafický design, na jehož výuce jsem měla příležitost se i podílet. V rámci designu mě baví proces, kdy ze zadání klienta vytvořím návrh, který zákazníka zkrátka nadchne. Když jsem dostala nabídku, abych se připojila ke KoPro, neváhala jsem. Mám radost, že díky designu, můžu dělat radost.
+    ',
     tag: 'Sarka'
   }
 
